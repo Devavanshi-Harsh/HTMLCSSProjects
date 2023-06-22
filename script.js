54baa98002b2a7c3d564c3ab1f9cc162342c5bb1
Harsh Devavanshi <85223811+Devavanshi-Harsh@users.noreply.github.com>
2023-06-22 15:02:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function clearForm() {
     document.getElementById("registration-form").reset();
+}
+function changeStylesheet(styleName) {
+  var i, linkTag;
+  var stylesheets = document.getElementsByTagName('link');
+
+  for (i = 0; i < stylesheets.length; i++) {
+    linkTag = stylesheets[i];
+    if (linkTag.getAttribute('rel').indexOf('stylesheet') !== -1) {
+      linkTag.disabled = true;
+      if (linkTag.getAttribute('title') === styleName) {
+        linkTag.disabled = false;
+      }
+    }
   }
-  
+}
